b5b5365c32e879acbf03219ae6770c2ad75faf00
Garib Mehdiyev <Garib.Mehdiyev@gmail.com>
2015-08-05 00:47:34 -0700

    Dashboard for QA teams who uses Stripe

---
+++ README.md
@@ -0,0 +1,1 @@
+# stripe-dashboard-for-qa
---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,26 @@
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show]
+  before_action :authenticate_user!
+
+  # GET /transactions
+  # GET /transactions.json
+  def index
+    @transactions = Stripe::BalanceTransaction.all(:limit => 20)
+  end
+
+  # GET /transactions/1
+  # GET /transactions/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_transaction
+      @transaction = Stripe::Charge.retrieve(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def transaction_params
+      params[:transaction]
+    end
+end
